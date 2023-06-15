bc113423aee487d266ab890a6f6f769e5b983f91
Shivani Rathor <120269305+Shivanirathor@users.noreply.github.com>
2023-06-15 17:59:32 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,40 @@
-const reducer = (state = 0, action) => {
-    switch (action.type) {
-        case "INCREMENT":
-            return state + 1;
-        case "DECREMENT":
-            return state - 1;
-        case "RESET":
-            return 0;
-        default:
-            return state;
-    }
+// const reducer = (state = 0, action) => {
+//     switch (action.type) {
+//         case "INCREMENT":
+//             return state + 1;
+//         case "DECREMENT":
+//             return state - 1;
+//         case "RESET":
+//             return 0;
+//         default:
+//             return state;
+//     }
+// };
+// export default reducer;
+
+import { createStore, combineReducers } from 'redux';
+
+// Reducer for counter state
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    case 'RESET':
+      return 0;
+    default:
+      return state;
+  }
 };
-export default reducer;
+
+// Root reducer
+const rootReducer = combineReducers({
+  counter: counterReducer,
+});
+
+// Create Redux store
+const store = createStore(rootReducer);
+
+export default store;
+
