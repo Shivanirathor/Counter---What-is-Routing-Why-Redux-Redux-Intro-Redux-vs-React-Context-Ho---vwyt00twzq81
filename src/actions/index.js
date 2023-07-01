0500a7e94e70c22576f38f858513d5562f020347
Shivani Rathor <120269305+Shivanirathor@users.noreply.github.com>
2023-07-01 17:32:36 +0530

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,3 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const RESET = 'RESET';
-
 export function increment() {
     return {
         type: "INCREMENT",
