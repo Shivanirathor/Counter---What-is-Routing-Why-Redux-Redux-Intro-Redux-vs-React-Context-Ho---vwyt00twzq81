409e62cacd95400e6b868fb5e508df41f089e018
Shivani Rathor <120269305+Shivanirathor@users.noreply.github.com>
2023-07-01 17:30:39 +0530

    Update counterContainer.js

--- src/components/counterContainer.js
+++ src/components/counterContainer.js
@@ -1,45 +1,24 @@
-// import { connect } from "react-redux";
-// import Counter from "./counter";
-// import { increment, decrement, reset } from "../actions";
+import { connect } from "react-redux";
+import Counter from "./counter";
+import { increment, decrement, reset } from "../actions";
 
-// const mapStateToProps = (state) => {
-//     return {
-//         counter: state,
-//     };
-// };
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//         increment: () => dispatch(increment()),
-//         decrement: () => dispatch(decrement()),
-//         reset: () => dispatch(reset()),
-//     };
-// };
+const mapStateToProps = (state) => {
+    return {
+        counter: state,
+    };
+};
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increment: () => dispatch(increment()),
+        decrement: () => dispatch(decrement()),
+        reset: () => dispatch(reset()),
+    };
+};
 
 // add code here
-import { connect } from 'react-redux';
-import { increment, decrement, reset } from '../actions';
-import Counter from './Counter';
-
-// Map Redux state to component props
-const mapStateToProps = (state) => {
-  return {
-    count: state.counter,
-  };
-};
-
-// Map action creators to component props
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increment: () => dispatch(increment()),
-    decrement: () => dispatch(decrement()),
-    reset: () => dispatch(reset()),
-  };
-};
-
-// Connect Redux store to the Counter component
 const CounterContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
+mapStateToProps,
+mapDispatchToProps
 )(Counter);
 
 export default CounterContainer;
